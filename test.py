a133883bfbafcf5711108cace721eb78ebf8b94d
jesselangdon <jesselangdon@gmail.com>
2017-03-14 09:01:13 -0700

    Added test parameters for solar_vector.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def test_copyRSFiles():
 test_copyRSFiles()
 
 
-#TEST solar_raster input variables
+# TEST solar_raster input variables
 in_dem = r"C:\JL\Testing\solar\Riverscapes\IDE_test\Inputs\dem_100m.tif"
 in_canopy = r"C:\JL\Testing\solar\Riverscapes\IDE_test\Inputs\nbcd_baw_100m.tif"
 in_stream = r"C:\JL\Testing\solar\Riverscapes\IDE_test\Inputs\segments.gdb\segments200m"
@@ -26,3 +26,14 @@ rs_bool = "true"
 wshd_name = "Entiat"
 rs_proj_name = "Entiat Solar Radiation"
 rs_real_name = "Test Run 01"
+
+# TEST solar_vector input variables
+in_raster = r"C:\JL\Projects\solar\Pahsimeroi\_2Model\area_solar.tif"
+in_stream = r"C:\JL\Projects\solar\Pahsimeroi\_1Sources\seg200m.gdb\seg200m_dslv_20170313"
+in_strm_indx = "LineOID"
+in_strm_area = r"C:\JL\Projects\solar\Pahsimeroi\_1Sources\Pahsimeroi_BF_Albers_merged"
+result = r"C:\JL\Projects\solar\Pahsimeroi\_2Model\pred_solar_20170313.shp"
+workspace_temp = r"C:\JL\Projects\solar\Pahsimeroi\scratch.gdb"
+rs_bool = "true"
+rs_dir = r"C:\JL\Projects\solar\Pahsimeroi\Riverscapes"
+rs_real_name = "NBCD 2016-182-243 Run"
